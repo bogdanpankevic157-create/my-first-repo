12fbcce2bc07713a4ff495b6f412c0289b9c6b9a
Bogdan Pankevich <bogdanpankevic157@gmail.com>
2026-08-19 00:43:58 +0300

    fix: update e2e test

--- tests/e2e.spec.js
+++ tests/e2e.spec.js
@@ -9,29 +9,32 @@ const { CheckoutCompletePage } = require('../pages/CheckoutCompletePage');
 test.describe('E2E: Полный цикл покупки', () => {
 
   test('Пользователь должен успешно купить самый дорогой товар', async ({ page }) => {
+    
     const loginPage = new LoginPage(page);
+    const inventoryPage = new InventoryPage(page);
+    const cartPage = new CartPage(page);
+    const checkoutStepOne = new CheckoutStepOnePage(page);
+    const checkoutStepTwo = new CheckoutStepTwoPage(page);
+    const checkoutComplete = new CheckoutCompletePage(page);
+
+    
     await loginPage.open();
     await loginPage.login('standard_user', 'secret_sauce');
 
-    const inventoryPage = new InventoryPage(page);
     await expect(inventoryPage.title).toHaveText('Products');
 
     const mostExpensiveItem = await inventoryPage.getMostExpensiveItemName();
     await inventoryPage.addItemToCart(mostExpensiveItem);
     await inventoryPage.openCart();
 
-    const cartPage = new CartPage(page);
     expect(await cartPage.isItemInCart(mostExpensiveItem)).toBe(true);
     await cartPage.goToCheckout();
 
-    const checkoutStepOne = new CheckoutStepOnePage(page);
     await checkoutStepOne.fillUserInfo('Test', 'User', '12345');
     await checkoutStepOne.continueCheckout();
 
-    const checkoutStepTwo = new CheckoutStepTwoPage(page);
     await checkoutStepTwo.finishCheckout();
 
-    const checkoutComplete = new CheckoutCompletePage(page);
     const message = await checkoutComplete.getCompletionMessage();
     expect(message).toBe('Thank you for your order!');
   });
